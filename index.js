b332f11303f8bcfa1ebac28a74e1cd5f04de519f
Judy Soukkhaphon <judysoukkhaphon@gmail.com>
2023-01-17 16:39:36 -0500

    resubmissiong from phase-0 to phase-1

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', function() {
+    alert('I was clicked!');
+    });
 }
+
+/* ABOVE DOESN'T WORK, BELOW DOES. Why doesn't code above work?
+
+function addingEventListener() {
+    
+    alert('I was clicked!');
+}
+
+const input = document.getElementById('button');
+input.addEventListener('click', addingEventListener);
+
+*/
